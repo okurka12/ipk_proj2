41591081b168707c3fe1012efcef1c0259818657
Vit Pavlik <pavlikvitek13@gmail.com>
2024-04-22 17:25:50 +0200

    update udp marker module

--- udp_marker.c
+++ udp_marker.c
@@ -16,6 +16,24 @@
 #include "udp_marker.h"
 #include "utils.h"
 
+#define WELCOME_INIT_SIZE 128
+
+/**
+ * private structure for udpm_welcome functions
+*/
+struct udpm_welcome {
+
+    /* this will be false for empty array slots*/
+    bool active;
+
+    uint16_t id;
+    uint32_t addr;
+    uint16_t port;
+};
+
+static struct udpm_welcome *welcome_data = NULL;
+static unsigned int welcome_len = 0;
+
 
 /**
  * return pointer to array (operate with udpm_mark or udpm_seen)
@@ -57,4 +75,51 @@ void udpm_dtor(int32_t **udpm_data) {
     if (udpm_data == NULL) return;
     free(*udpm_data);
     *udpm_data = NULL;
+}
+
+
+void udpm_welcome_mark(uint16_t id, uint32_t addr, uint16_t port) {
+
+    if (welcome_len == 0) {
+
+        welcome_data =
+            calloc(WELCOME_INIT_SIZE * sizeof(struct udpm_welcome), 1);
+
+        welcome_len = WELCOME_INIT_SIZE;
+
+        welcome_data[0].active = true;
+        welcome_data[0].id = id;
+        welcome_data[0].addr = addr;
+        welcome_data[0].port = port;
+        return;
+    }
+
+    for (unsigned int i = 0; i < welcome_len; i++) {
+        if (welcome_data[i].active) continue;
+        welcome_data[i].active = true;
+        welcome_data[i].id = id;
+        welcome_data[i].addr = addr;
+        welcome_data[i].port = port;
+        return;
+    }
+    log(WARNING, "expanding the welcoma data array was not yet implemented "
+    "(probably too many UDP clients since starting the server)");
+    /* todo */
+}
+
+bool udpm_welcome_seen(uint16_t id, uint32_t addr, uint16_t port) {
+    for (unsigned int i = 0; i < welcome_len; i++) {
+        bool c1 = welcome_data[i].active;
+        bool c2 = welcome_data[i].id == id;
+        bool c3 = welcome_data[i].addr == addr;
+        bool c4 = welcome_data[i].port == port;
+        if (c1 and c2 and c3 and c4) return true;
+    }
+    return false;
+}
+
+void udpm_welcome_free_res() {
+    welcome_len = 0;
+    free(welcome_data);
+    welcome_data = NULL;
 }
--- udp_marker.h
+++ udp_marker.h
@@ -33,6 +33,27 @@ bool udpm_seen(uint16_t id, int32_t *udpm_data);
 */
 void udpm_dtor(int32_t **udpm_data);
 
+/**
+ * the server welcome socket needs to differentiate between ids from different
+ * addresses. Hence, it will use this function to mark `id` from `address`
+ * and `port` as seen
+ *
+ * That way, it can receive AUTH message id=0 from 127.0.0.1:12345
+ * but also AUTH message id=0 from 127.0.0.1:34343
+ *
+ * @note this function has static data, so there can be only one instance
+ * of seen data, it will grow as the server operates and accepts udp clients
+ *
+ * @note caller has no way of knowing if the function completed successfully,
+ * but given that not much will happen if it doesn't, it's a risk im
+ * willing to take
+*/
+void udpm_welcome_mark(uint16_t id, uint32_t addr, uint16_t port);
+
+bool udpm_welcome_seen(uint16_t id, uint32_t addr, uint16_t port);
+
+void udpm_welcome_free_res();
+
 
 
 #endif  // ifndef _U_D_P_M_A_R_K_E_R_H_
